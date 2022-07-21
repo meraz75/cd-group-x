c4b4125640ab22b972ec77da93f42af12e8372f4
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2022-07-21 12:18:00 +0600

    Finished Kamruzzaman's part of the task.

--- main.cpp
+++ main.cpp
@@ -16,115 +16,10 @@ void validexp(string ex){
         if(str==s1 or str==s2)cout<<"status: valid"<<endl;
         else cout<<"status: invalid"<<endl;
     }
-        //star
-    else if(ex[1]=='*'){
-        if(esz==2){
-            cout<<"Enter a string to verify [$ for empty string]: ";
-            cin>>str;
-            int sz=str.size();
-            if(str[0]=='$')cout<<"status: valid"<<endl;
-            else{
-                for(int i=0;i<sz;i++){
-                    if(str[i]!=ex[0]){
-                        f=0;
-                        break;
-                    }
-                }
-                if(f)cout<<"status: valid"<<endl;
-                else cout<<"status: invalid"<<endl;
-            }
-        }
-        else if(esz>2 and ex[2]!='U' and ex[2]!='+' and ex[2]!='*'){
-            cout<<"Enter a string to verify [$ for empty string]: ";
-            cin>>str;
-            int sz=str.size();
-            sz--;
-            if(sz==0 and str[0]==ex[2])cout<<"status: valid"<<endl;
-            else{
-                if(str[sz]!=ex[2])f=0;
-                if(f){
-                    for(int i=0;i<sz;i++){
-                        if(str[i]!=ex[0]){
-                            f=0;
-                            break;
-                        }
-                    }
-                }
-                if(f)cout<<"status: valid"<<endl;
-                else cout<<"status: invalid"<<endl;
-            }
-        }
-        else cout<<"status: invalid expression"<<endl;
-    }
-        //star
-    else if(esz==3 and ex[2]=='*' and ex[1]!='U' and ex[1]!='+' and ex[1]!='*'){
-        cout<<"Enter a string to verify [$ for empty string]: ";
-        cin>>str;
-        int sz=str.size();
 
-        if(str[0]!=ex[0])f=0;
-        if(f){
-            for(int i=1;i<sz;i++){
-                if(str[i]!=ex[1]){
-                    f=0;
-                    break;
-                }
-            }
-        }
-        if(f)cout<<"status: valid"<<endl;
-        else cout<<"status: invalid"<<endl;
-    }
-        //plus
-    else if(ex[1]=='+'){
 
-        if(esz==2){
-            cout<<"Enter a string to verify [$ for empty string]: ";
-            cin>>str;
-            int sz=str.size();
-            for(int i=0;i<sz;i++){
-                if(str[i]!=ex[0]){
-                    f=0;
-                    break;
-                }
-            }
-            if(f)cout<<"status: valid"<<endl;
-            else cout<<"status: invalid"<<endl;
-        }
-        else if(esz>2 and ex[2]!='*' and ex[2]!='U' and ex[2]!='+'){
-            cout<<"Enter a string to verify [$ for empty string]: ";
-            cin>>str;
-            int sz=str.size();
-            for(int i=0;i<sz-1;i++){
-                if(str[i]!=ex[0]){
-                    f=0;
-                    break;
-                }
-            }
-            if(f and str[sz-1]==ex[2])cout<<"status: valid"<<endl;
-            else cout<<"status: invalid"<<endl;
-        }
-        else cout<<"status: invalid expression"<<endl;
-    }
-        //plus
-    else if(esz==3 and ex[2]=='+' and ex[1]!='*' and ex[1]!='U' and ex[1]!='+'){
-        cout<<"Enter a string to verify [$ for empty string]: ";
-        cin>>str;
-        int sz=str.size();
 
-        if(ex[0]!=str[0] or sz==1)f=0;
-        if(f){
-            for(int i=1;i<sz;i++){
-                if(str[i]!=ex[1]){
-                    f=0;
-                    break;
-                }
-            }
-        }
-        if(f)cout<<"status: valid"<<endl;
-        else cout<<"status: invalid"<<endl;
-    }
-    else if(ex[0]=='U' or ex[0]=='*' or ex[0]=='+')cout<<"status: invalid expression"<<endl;
-    else cout<<"status: invalid expression"<<endl;
+
 }
 
 
@@ -136,5 +31,4 @@ int main() {
     cin >> input;
     validexp(input);
     return 0;
-
 }
